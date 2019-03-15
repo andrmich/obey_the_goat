7f45d51661b6bd0b9134f94ccacd53f1cb0969ff
Michalina_Andrzejewska <andrmichgithub@mismail.xyz>
2019-03-15 15:19:32 +0100

    Hack FT runner to be able to test staging

--- superlists/settings.py
+++ superlists/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -72,4 +72,4 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
