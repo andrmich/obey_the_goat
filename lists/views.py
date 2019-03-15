b2b9692f5f8a3398671d929c193f55b5952aaa86
Michalina_Andrzejewska <andrmichgithub@mismail.xyz>
2019-03-15 14:34:13 +0100

    Use Bootstrap to improve layout

--- lists/views.py
+++ lists/views.py
@@ -10,12 +10,6 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    #items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'list': list_})
-
-
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
@@ -28,3 +22,7 @@ def add_item(request, list_id):
     return redirect(f'/lists/{list_.id}/')
 
 
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    # items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
--- superlists/settings.py
+++ superlists/settings.py
@@ -72,3 +72,4 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+
